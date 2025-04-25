736c940129a3dcb9268dd5361758baa4ccc1ff1f
Felipe <felip.goncalves@hotmail.com>
2025-04-24 22:27:59 -0300

    Adicionando lógica de manipulação de formulário e configuração de data com dayjs

---
+++ src/modules/form/submit.js
@@ -0,0 +1,18 @@
+"use strict"
+import dayjs from "dayjs";
+
+const form = document.querySelector("form");
+const selectedDate = document.getElementById("date");
+
+// Data atual para o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
+
+// Carrega a data atual e data mínima como sendo a data atual.
+selectedDate.value = inputToday;
+selectedDate.min = inputToday;
+
+form.onsubmit = (event) => {
+    // Previne o comportamento padrão de carregar a página. 
+    event.preventDefault();
+
+}
