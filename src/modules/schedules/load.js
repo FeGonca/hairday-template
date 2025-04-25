2448decf3f9d71676b29824d09199adfae0e3f39
Felipe <felip.goncalves@hotmail.com>
2025-04-25 08:53:34 -0300

    Atualizando a função hoursLoad para incluir agendamentos diários e melhorar a lógica de disponibilidade de horários

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -18,5 +18,5 @@ export async function schedulesDay() {
     scheduleShow({ dailySchedules })
     
     // Rederiza as horas disponíveis.
-    hoursLoad({date});
+    hoursLoad({ date, dailySchedules });
 }
