8a68a543801ec2ac03456c2c12447988e8ee17b7
Felipe <felip.goncalves@hotmail.com>
2025-04-25 07:01:14 -0300

    Adicionando lógica para recuperar e validar o nome do cliente no formulário de agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@
 import dayjs from "dayjs";
 
 const form = document.querySelector("form");
+const clienteName = document.getElementById("client");
 const selectedDate = document.getElementById("date");
 
 // Data atual para o input
@@ -14,5 +15,42 @@ selectedDate.min = inputToday;
 form.onsubmit = (event) => {
     // Previne o comportamento padrão de carregar a página. 
     event.preventDefault();
+
+    try {
+        // Recuperando o nome do cliente.
+        const name = clienteName.value.trim();
+        
+        if (!name) {
+            return alert("informe o nome do cliente!")
+        }
+
+        // Recuperar o horário selecionado.
+        const hourSelected = document.querySelector(".hour-selected");
+
+        if (!hourSelected) {
+            return alert("Selecione a hora")
+        }
+
+        // Recuperar somente a hora
+        const [hour] = hourSelected.innerText.split(":")
+
+        // Insere a hora na data
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+
+        // Gera um ID
+        const id = new Date().getTime()
+
+        console.log(
+            id,
+            name,
+            when,
+        );
+        
+        
+        
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento.")
+        console.log(error);
+    }
 
 }
